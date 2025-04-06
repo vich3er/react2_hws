3a95ae364a2b8409489d08f11ff72bec29070ef9
vich3er <cherednikviktoriya@gmail.com>
2025-04-07 00:17:21 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
 function App() {
-
+    const coursesAndDurationArray: {title: string, monthDuration: number}[] = [
+        {title: 'JavaScript Complex', monthDuration: 5},
+        {title: 'Java Complex', monthDuration: 6},
+        {title: 'Python Complex', monthDuration: 6},
+        {title: 'QA Complex', monthDuration: 4},
+        {title: 'FullStack', monthDuration: 7},
+        {title: 'Frontend', monthDuration: 4}
+    ];
 
   return (
       <>
-
+          {
+              coursesAndDurationArray.map((course, i) =>{
+                  return (
+                       <div  className={'border-blue-400 border-1 p-1 mb-2 pb-2 pt-2  rounded-sm'} key={i}><p className={"inline"}>{course.title}</p> <p className={'inline'}>{course.monthDuration}</p></div>
+                  )
+              })
+          }
       </>
   )
 }
