919fbaf455fb48b10738447b87b90a13bf443cfb
vich3er <cherednikviktoriya@gmail.com>
2025-04-07 00:03:57 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import './App.css'
 
 function App() {
 
-
+    const coursesTitleArray: string[] = [
+        'JavaScript Complex',
+        'Java Complex',
+        'Python Complex',
+        'QA Complex',
+        'Fullstack',
+        'Frontend'
+    ];
   return (
       <>
-
+          {coursesTitleArray.map((title) => <p className={'font-bold text-red-600 p-1'}>{title}</p>)}
       </>
   )
 }
